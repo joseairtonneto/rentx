59aff12ad82f614667514a9f5537c39c5d39b043
José Airton Neto <jose.neto.airton@gmail.com>
2021-08-10 00:10:33 -0300

    Iniciando a documentação

--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -27,6 +27,7 @@ class ImportCategoriesUseCase {
           categories.push({ name, description });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (error) => {
